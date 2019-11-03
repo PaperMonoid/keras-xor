015d5505a09858d7ec81e22be22844d88667dc83
papermonoid <daniel.santiago.aguila.torres@gmail.com>
2019-11-02 22:46:46 -0700

    add: all the code

---
+++ README.md
@@ -0,0 +1,23 @@
+# Keras XOR
+Simple XOR model with keras.
+
+## How to run
+```
+pip install -r requirements.txt
+python .
+```
+
+## Output
+```
+INPUTS
+[[0 0]
+ [0 1]
+ [1 0]
+ [1 1]]
+
+OUTPUTS
+[[0.9900932 ]
+ [0.00891084]
+ [0.00973855]
+ [0.99114066]]
+```
---
+++ __main__.py
@@ -0,0 +1,38 @@
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+import numpy as np
+
+# create model with 2 inputs and 1 output
+model = Sequential()
+
+model.add(Dense(2, input_shape=(2,)))
+model.add(Activation("sigmoid"))
+
+model.add(Dense(3))
+model.add(Activation("sigmoid"))
+
+model.add(Dense(1))
+model.add(Activation("sigmoid"))
+
+# use loss function for binary classification
+model.compile(optimizer="rmsprop", loss="binary_crossentropy", metrics=["accuracy"])
+
+# training data
+data = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+labels = np.array([1, 0, 0, 1])
+
+# train model
+model.fit(data, labels, epochs=5000, batch_size=4)
+
+# test data
+inputs = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+
+# predict classes
+classes = model.predict(inputs)
+
+# print results
+print("INPUTS")
+print(inputs)
+
+print("OUTPUTS")
+print(classes)
